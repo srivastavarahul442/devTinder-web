3f1d4ea18786ff7941d08237e4fe16850d50a582
srivastavarahul442 <srivastavarahul442@gmail.com>
2025-01-02 17:36:15 +0530

    working on redux

---
+++ src/components/Login.jsx
@@ -0,0 +1,90 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("rahul@gmail.com");
+  const [password, setPassword] = useState("Rahul@#0314");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try{
+        const res = await axios.post(BASE_URL+"/login",{
+            emailId,
+            password
+        },
+        {
+            withCredentials:true
+        });
+        // console.log(res)
+        dispatch(addUser(res.data))
+        return navigate("/")
+    }
+    catch(err){
+        console.error("Error : "+err.message)
+    }
+  }
+
+  return (
+    <div>
+      <div className="card bg-base-300 w-96 shadow-xl flex mx-auto my-10">
+        <div className="card-body">
+          <h2 className="card-title justify-center text-2xl">Login</h2>
+          <div>
+            <label className="input input-bordered flex items-center gap-2 my-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+              </svg>
+              <input
+                type="text"
+                className="grow"
+                placeholder="Email"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </label>
+            <label className="input input-bordered flex items-center gap-2 my-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input
+                type="password"
+                className="grow"
+                placeholder="Password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary w-40 text-xl mx-auto my-1" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import { useSelector } from "react-redux";
+
+const Navbar = () => {
+  const user = useSelector((store) => store.user);
+  console.log("navbar");
+  console.log(user);
+  return (
+    <div className="navbar bg-base-300">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">👩‍💻CodeMate👨‍💻</a>
+      </div>
+      <div className="flex-none gap-2">
+        {user && <p>Welcome, {user.firstName}</p>}
+        <div className="dropdown dropdown-end mx-5">
+          {user && (
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src={user.photoUrl}
+                />
+              </div>
+            </div>
+          )}
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
