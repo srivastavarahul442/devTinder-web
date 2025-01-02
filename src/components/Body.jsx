019eb6a9bf60f7d587336eed2483d67463b4df35
srivastavarahul442 <srivastavarahul442@gmail.com>
2025-01-02 20:14:44 +0530

    build Authentication for login(without login not able to access any pages)

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,47 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
-    return(
-        <div>
-            <Navbar />
-            <Outlet />
-            <Footer />
-        </div>
-    )
-}
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchData = async () => {
+    if(userData){
+        return
+      }
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      console.log(res.data);
+
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.error("Error : " + err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 export default Body;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
   const user = useSelector((store) => store.user);
-  console.log("navbar");
-  console.log(user);
+  
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">👩‍💻CodeMate👨‍💻</a>
+        <Link to="/" className="btn btn-ghost text-xl">👩‍💻CodeMate👨‍💻</Link>
       </div>
       <div className="flex-none gap-2">
         {user && <p>Welcome, {user.firstName}</p>}
@@ -31,10 +32,10 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a className="justify-between">
+              <Link to={"/profile"} className="justify-between">
                 Profile
                 <span className="badge">New</span>
-              </a>
+              </Link>
             </li>
             <li>
               <a>Settings</a>
